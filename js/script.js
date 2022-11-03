fe040c80483f32e99b575432ac81a8521a2068b2
LudwigVonEffort <dr.feynman65@gmail.com>
2022-11-04 00:05:19 +0100

    add btn guide script

--- js/script.js
+++ js/script.js
@@ -4,13 +4,15 @@ const eleLevelSelect = document.querySelector('#level');
 const eleWelcome = document.querySelector('.welcome');
 const eleGrid = document.querySelector('.grid');
 
+const eleGuideButton = document.querySelector('#btn-guide')
+
 elePlayButton.addEventListener('click', function(){
     eleGrid.innerHTML = ''; // this is for reset of the form output
 
     eleGrid.classList.remove('hidden');
     eleWelcome.classList.add('hidden');
      
-    const nCell = parseInt(eleLevelSelect.value);;
+    const nCell = parseInt(eleLevelSelect.value);
     const sideSquare = Math.sqrt(nCell);
     eleGrid.style.setProperty('--sideSquare', sideSquare);
 
@@ -31,5 +33,19 @@ elePlayButton.addEventListener('click', function(){
     }
 });
 
+eleGuideButton.addEventListener('click', function () {
 
+    if (eleGuideButton.innerHTML == 'Guide') {
+        eleGuideButton.innerHTML = 'Resume';
+        eleGrid.classList.add('hidden');
+        eleWelcome.classList.remove('hidden');
+    }
+
+    else {
+        eleGuideButton.innerHTML = 'Guide';
+        eleGrid.classList.remove('hidden');
+        eleWelcome.classList.add('hidden');
+    }
+
+});
 
